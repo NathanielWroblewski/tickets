3e1d5684066597d80dc8e91b29d4125106ddfadc
Jared Smith <jaredsmithse@icloud.com>
2013-09-21 16:52:25 -0700

    Add class method to grab all tickets that happen at the locations supplied through the array, #at_locations(locations)

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -8,4 +8,12 @@ class Ticket < ActiveRecord::Base
     :description,
     :blog_url,
     :location
+
+  def self.at_locations(locations)
+  	tickets_at_location = []
+		locations.each do |location|
+			tickets_at_location << Ticket.where(location: location).to_a
+		end
+		tickets_at_location
+  end
 end
