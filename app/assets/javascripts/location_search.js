ab340b4600a7ebf3bb90a3326d07fae97c387822
Jared Smith <jaredsmithse@icloud.com>
2013-09-21 21:54:05 -0700

    JS for requesting lat and long for a location query string

---
+++ app/assets/javascripts/location_search.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$('.pin-it').on('submit', function(e) {
+		e.preventDefault();
+		var query = $('.pin-it #location').val()
+		$.ajax({
+        url: '/search_query',
+        type: 'POST',
+        data: { query: query }
+      }).done(function(data){
+        
+    });
+	});
+
+});
