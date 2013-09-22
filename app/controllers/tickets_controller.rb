7009cf165f5c50b4dea106d251978d1392c7bf6b
Nathaniel Wroblewski <nathanielwroblewski@gmail.com>
2013-09-21 21:21:24 -0700

    adding itinerary

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,7 +1,17 @@
 class TicketsController < ApplicationController
 
   def create
-    Ticket.create(params[:ticket])
+    p params
+    ticket = Ticket.new(params[:ticket])
+    if ticket.save
+      render json: {
+        title: ticket.title,
+        location: ticket.location,
+        description: ticket.description
+      }
+    else
+      render json: { error: 'There was a problem saving your event.' }
+    end
   end
 
 end
