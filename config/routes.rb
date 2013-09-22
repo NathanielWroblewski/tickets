852397be3c3a6d05ff3f01d79407f55aad680281
Jared Smith <jaredsmithse@icloud.com>
2013-09-22 02:27:34 -0700

    Enable all ticket resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Prop::Application.routes.draw do
   resources :users, only: [:new, :create]
   resources :itineraries, only: [:index, :show]
   resources :pages, only: [:index, :show]
-  resources :tickets, only: :create
+  resources :tickets#, only: :create
   resources :sessions, only: [:create, :delete, :new]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
