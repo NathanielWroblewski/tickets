f3e3afaffce6cddbbebb04104f5ab5e084c80f22
Jared Smith <jaredsmithse@icloud.com>
2013-09-21 20:17:12 -0700

    Added special case handling for UAE

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -20,11 +20,17 @@ class ItinerariesController < ApplicationController
     
     # the api key is hardcoded in, i blame this on Nate
     response = HTTParty.post("http://www.geocodefarm.com/api/reverse/json/65caadac07b171d25d08af0153a382022f05129e/#{lat}/#{long}/")
-    results = JSON.parse(response.body)['geocoding_results']['ADDRESS']['address'].split(', ')
+    results = JSON.parse(response.body)['geocoding_results']['ADDRESS']['address']
     if results.include? "USA"
+    	results = results.split(", ")
     	location_info[:city] = results[-3]
     	location_info[:region] = results[-2].split(" ")[0]
+    elsif results.include? "United Arab Emirates"
+    	results = results.split(", ")
+    	location_info[:city] = results[-1].split(" - ")[-2]
+    	location_info[:region] = "UAE"
     else
+    	results = results.split(", ")
     	location_info[:city] = results[-2]
     	location_info[:region] = results[-1]
     end
