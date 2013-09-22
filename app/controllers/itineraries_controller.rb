e68c74ae2d37365b33d21499726508aaf6d72bde
Jared Smith <jaredsmithse@icloud.com>
2013-09-21 19:42:01 -0700

    Add reverse geocoding and handling of response to be passed back in json

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -11,7 +11,20 @@ class ItinerariesController < ApplicationController
   def save_lat_long
     lat = params[:lat].to_f
     long = params[:long].to_f
-    render nothing: true
+    location_info = {}
+    
+    # the api key is hardcoded in, i blame this on Nate
+    response = HTTParty.post("http://www.geocodefarm.com/api/reverse/json/65caadac07b171d25d08af0153a382022f05129e/#{lat}/#{long}/")
+    results = JSON.parse(response.body)['geocoding_results']['ADDRESS']['address'].split(', ')
+    if results.include? "USA"
+    	location_info[:city] = results[-3]
+    	location_info[:region] = results[-2].split(" ")[0]
+    else
+    	location_info[:city] = results[-2]
+    	location_info[:region] = results[-1]
+    end
+
+    render json: location_info
   end
 
 end
