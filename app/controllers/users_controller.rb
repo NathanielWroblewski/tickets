033bcb4cdded1f3d8782e1c30d7636db84b7b4ee
Yaron <ysadka@gmail.com>
2013-09-21 19:27:48 -0700

    create and new actions for user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,21 @@
 class UsersController < ApplicationController
   def new
-
+    @user = User.new
   end
 
   def create
+    @user = User.new(params[:user])
+    if @user.save
+      session[:id] = @user.id
 
+
+######
+      redirect_to @user #WHERE SHOULD THIS REDIRECT TO?????
+#######
+
+    else
+      render :new
+    end
   end
 
 end
